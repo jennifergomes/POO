7c422f2dd1f86bff681a3c9d2cdc620bac8d3b08
Jennifer Gomes <82291497+jennifergomes@users.noreply.github.com>
2023-10-26 21:26:37 -0300

    Add files via upload

---
+++ Exemplo_Circulo/src/Circulo/Ponto.java
@@ -0,0 +1,28 @@
+package Circulo;
+
+//Exemplo de composição
+
+public class Ponto {
+    private float x, y;
+
+    public Ponto(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void alterarX(float x){
+        this.x = x;
+    }
+
+    public void alterarY(float y){
+        this.y = y;
+    }
+
+    public float obterX(){
+        return x;
+    }
+
+    public float obterY(){
+        return y;
+    }
+}
