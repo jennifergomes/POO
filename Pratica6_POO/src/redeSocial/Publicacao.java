db73fd877a3f5abc3bc6ed39a178fbebb204eac7
Jennifer Gomes <82291497+jennifergomes@users.noreply.github.com>
2023-10-26 21:50:34 -0300

    Add files via upload

---
+++ Pratica6_POO/src/redeSocial/Publicacao.java
@@ -0,0 +1,36 @@
+package redeSocial;
+
+public class Publicacao {
+
+    private String dataPublicacao, textoPublicacao, linkMidia;
+
+    public Publicacao(String dataPublicacao, String textoPublicacao, String linkMidia) {
+        this.dataPublicacao = dataPublicacao;
+        this.textoPublicacao = textoPublicacao;
+        this.linkMidia = linkMidia;
+    }
+
+    public String getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(String dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public String getTextoPublicacao() {
+        return textoPublicacao;
+    }
+
+    public void setTextoPublicacao(String textoPublicacao) {
+        this.textoPublicacao = textoPublicacao;
+    }
+
+    public String getLinkMidia() {
+        return linkMidia;
+    }
+
+    public void setLinkMidia(String linkMidia) {
+        this.linkMidia = linkMidia;
+    }
+}
---
+++ Pratica6_POO/src/redeSocial/RedeSocial.java
@@ -0,0 +1,45 @@
+package redeSocial;
+
+public class RedeSocial {
+
+    private String dataCriacao, nomeUsuario, dataNascimento, senha;
+
+    public RedeSocial(String dataCriacao, String nomeUsuario, String dataNascimento, String senha) {
+        this.dataCriacao = dataCriacao;
+        this.nomeUsuario = nomeUsuario;
+        this.dataNascimento = dataNascimento;
+        this.senha = senha;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
